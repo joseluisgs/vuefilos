aefa6897c5a3ea95aec644afffa1ca06edb6049a
joseluisgs <joseluisgonsan@gmail.com>
2020-07-06 13:02:12 +0200

    Navegacion

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
+    <Navegacion></Navegacion>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navegacion from '@/components/Navegacion.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navegacion,
+  },
+};
+</script>
 
 <style lang="scss">
 </style>
---
+++ src/components/Navegacion.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-navbar fixed-top="true" type="is-dark">
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="@/assets/vuefilos.png"
+                    alt="Vuefilos logo"
+                >
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="/">
+                Portada
+            </b-navbar-item>
+            <!-- <b-navbar-item href="#">
+                Documentation
+            </b-navbar-item>
+            <b-navbar-dropdown label="Info">
+                <b-navbar-item href="#">
+                    About
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Contact
+                </b-navbar-item>
+            </b-navbar-dropdown> -->
+        </template>
+
+        <template slot="end">
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                  <router-link class="button is-primary" to="/registro">
+                    <strong>Registro</strong>
+                  </router-link>
+                  <router-link class="button is-light" to="/login">Login</router-link>
+                  <router-link class="button is-light" to="/perfil">Perfil</router-link>
+                  <a class="button is-light" href="#">Logout</a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
