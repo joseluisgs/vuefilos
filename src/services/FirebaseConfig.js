32d6608ca008f68e6c3f96c6791d0f74ce57f175
joseluisgs <joseluisgonsan@gmail.com>
2020-07-12 17:59:51 +0200

    to heroku

---
+++ src/services/FirebaseConfig.js
@@ -0,0 +1,23 @@
+// Configuración de firebase si lo quieresponer oculto
+// renombra este fichero como FirebaseConfig.js
+// https://firebase.google.com/docs/web/setup?hl=es
+export default {
+  apiKey: 'AIzaSyAismu2WKwjqXuqj3Qs2pTLR0gnVi4plN8',
+  authDomain: 'vuefilos-fire.firebaseapp.com',
+  databaseURL: 'https://vuefilos-fire.firebaseio.com',
+  projectId: 'vuefilos-fire',
+  storageBucket: 'vuefilos-fire.appspot.com',
+  messagingSenderId: '666107992454',
+  appId: '1:666107992454:web:475ce6a0cae04a83b63407',
+};
+
+// Configuramos Firebase desde env
+// const firebaseConfig = {
+//   apiKey: process.env.VUE_APP_FIRE_API_KEY,
+//   authDomain: process.env.VUE_APP_FIRE_AUTH_DOMAIN,
+//   databaseURL: process.env.VUE_APP_FIRE_DATABASE_URL,
+//   projectId: `${process.env.VUE_APP_FIRE_PROJECT_ID}`,
+//   storageBucket: process.env.VUE_APP_FIRE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.VUE_APP_FIRE_MESSAGING_SENDER_ID,
+//   appId: process.env.VUE_APP_FIRE_APP_ID,
+// };
